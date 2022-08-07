6909df0488d1d48c728b0e93f21405d18fd27c6f
FAIZOKTAVIANDI <deltharashna@gmail.com>
2022-08-07 10:50:13 +0700

    change port

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
  
-app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
+app.listen(process.env.PORT || 5001, ()=> console.log('Server up and running...'));
